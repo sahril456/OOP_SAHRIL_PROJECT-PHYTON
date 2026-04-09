8ef5e0afc7f360fa5bf441ea4cfe2efd039741df
MAULANA SAHRIL HIDAYAH <maulanasyahril208@gmail.com>
2026-04-09 07:59:35 +0700

    Add files via upload

---
+++ try4_oop.py
@@ -0,0 +1,15 @@
+a=10
+b=3
+
+print ("X =a+b")
+print ("x =",a+b)
+print ("Y =a-b")
+print ("Y=",a-b)
+print ("C=a*b")
+print ("C=",a*b)
+print ("Z=a/b")
+print ("Z=",a/b)
+print ("p=a**b")
+print ("p=",a**b)
+print ("W=a%b")
+print ("W=",a%b)
